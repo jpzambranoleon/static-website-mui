fcfddb3189430a8a3dba6fbfdfc208af0fa60d76
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-02 16:48:25 -0500

    updated Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const Navbar = () => {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h6" color="inherit" noWrap>
+                    leone digitale
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar;
