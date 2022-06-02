3d208d69ed7649633369bdc084cac3aa28fdae07
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-02 17:31:04 -0500

    created TheTeam.jsx

---
+++ src/home/components/TheTeam.jsx
@@ -0,0 +1,26 @@
+import { Card, Grid } from "@mui/material"
+
+const TheTeam = () => {
+    return (
+        <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={3}>
+                <Card
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                >
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>  
+    )
+}
