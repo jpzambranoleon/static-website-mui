277959c811235104f4c025acec1c389606109669
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-03 00:49:12 -0500

    updated files

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,36 +1,37 @@
 import { Box, Button, Container, Typography, Stack } from "@mui/material";
+import About from "./components/About";
 import TheTeam from "./components/TheTeam";
 
 export default function Home() {
     return (
         <main>
-        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              The Team
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
+        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+            <About />
+            <Container maxWidth="sm">
+                <Typography
+                    component="h1"
+                    variant="h2"
+                    align="center"
+                    color="text.primary"
+                    gutterBottom
+                >
+                    The Team
+                </Typography>
+                <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                    Something short and leading about the collection below—its contents,
+                    the creator, etc. Make it short and sweet, but not too short so folks
+                    don&apos;t simply skip over it entirely.
+                </Typography>
+                <Stack
+                    sx={{ pt: 4 }}
+                    direction="row"
+                    spacing={2}
+                    justifyContent="center"
+                >
+                    <Button variant="contained">Main call to action</Button>
+                    <Button variant="outlined">Secondary action</Button>
+                </Stack>
+            </Container>
         </Box>
         <Container sx={{ py: 8 }}>
           {/* End hero unit */}
--- src/home/components/About.jsx
+++ src/home/components/About.jsx
@@ -1,21 +1,46 @@
-import { CardMedia, Grid, Typography } from "@mui/material";
+import { CardMedia, Container, Grid, Typography } from "@mui/material";
 
 const About = () => {
     return (
-        <Grid container>
-            <Grid item sx={12} sm={8}>
-                <Typography>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic, alias deleniti quo ipsum porro quod enim obcaecati accusamus cumque praesentium assumenda laborum ducimus eaque, ad nemo iusto ab repudiandae sed?
-                </Typography>
+        <Container>
+            <Grid container spacing={4}>
+                <Grid item sx={12} sm={8}>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="left"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Turning Ideas
+                    </Typography>
+                    <Typography
+                        component="h1"
+                        variant="h2"
+                        align="left"
+                        color="text.primary"
+                        gutterBottom
+                    >
+                        Into Realities
+                    </Typography>
+                    <Typography variant="h5" align="left" color="text.secondary" paragraph>
+                        We are a team of college friends who decided to form a company on the
+                        basis of brining our ideas to life. We come from different parts of the
+                        world, but we have one thing in common: our love of technology. Which is
+                        why we decided to build a company that follows the latest trends in 
+                        computer programing, machine learning and cryptocurrency. We may be a start-up,
+                        but we have big plans for the future.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <CardMedia 
+                        component="img"
+                        image="https://unsplash.com/photos/HVp2MG15y3E"
+                        alt="random"
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-                <CardMedia 
-                    component="img"
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                />
-            </Grid>
-        </Grid>
+        </Container>
     )
 }
 
