c31eb58aabc528bff9ae5965dfc83d36e22db3f6
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-03 11:53:59 -0500

    created FuturePlans.jsx

---
+++ src/home/components/FuturePlans.jsx
@@ -0,0 +1,24 @@
+import { Container, Grid } from "@mui/material";
+
+const FuturePlans = () => {
+    return (
+        <Container>
+            <Typography
+                component="h1"
+                variant="h6"
+                align="center"
+                color="secondary"
+                gutterBottom
+            >
+                FUTURE PLANS?
+            </Typography>
+            <Grid container>
+                <Grid item sx={12} sm={8}>
+                    
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default FuturePlans;
