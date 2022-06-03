1b55521b33bffa8f57c869a965b76e4f5f743d0f
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-03 12:03:12 -0500

    updated FuturePlans.jsx

--- src/home/components/FuturePlans.jsx
+++ src/home/components/FuturePlans.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from "@mui/material";
+import { Card, CardMedia, Container, Grid, Typography } from "@mui/material";
 
 const FuturePlans = () => {
     return (
@@ -15,6 +15,33 @@ const FuturePlans = () => {
             <Grid container>
                 <Grid item sx={12} sm={8}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolor dicta, nostrum sequi pariatur consequuntur aspernatur esse aperiam omnis molestias voluptas? Sit ut alias, dolores earum reprehenderit facere blanditiis. Pariatur.
+                    <Grid container>
+                        <Grid item sx={12} sm={4}>
+                            <Typography>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aut sapiente, sed perferendis harum accusamus ab doloribus quae adipisci totam ex omnis labore doloremque modi, sint vitae ad quod autem.
+                            </Typography>
+                        </Grid>
+                        <Grid item sx={12} sm={4}>
+                            <Typography>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aut sapiente, sed perferendis harum accusamus ab doloribus quae adipisci totam ex omnis labore doloremque modi, sint vitae ad quod autem.
+                            </Typography>
+                        </Grid>
+                        <Grid item sx={12} sm={4}>
+                            <Typography>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit aut sapiente, sed perferendis harum accusamus ab doloribus quae adipisci totam ex omnis labore doloremque modi, sint vitae ad quod autem.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <Card>
+                        <CardMedia 
+                            component="img"
+                            maxHeight="100%"
+                            image="/images/slick_laptop.jpg"
+                            alt="random"
+                        />
+                    </Card>
                 </Grid>
             </Grid>
         </Container>
