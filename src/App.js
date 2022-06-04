897f60317f05e77aa55134a3d212c3014c6a8f41
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-04 17:30:28 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./home/Home";
 
@@ -5,6 +6,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
       <Home />
     </div>
   );
