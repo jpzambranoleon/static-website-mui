b0585ebc2766df4580b1a819a5df0e9125fd8e08
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-04 18:22:20 -0500

    updated Content.jsx

--- src/pages/error404/components/Content.jsx
+++ src/pages/error404/components/Content.jsx
@@ -13,32 +13,32 @@ const Content = () => {
             <Grid container>
                 <Grid item sx={12} sm={6}>
                     <Box sx={{ pt: 10 }}>
-                    <Typography
-                        component="h1"
-                        variant="h1"
-                        align="left"
-                        color="text.primary"
-                    >
-                        404
-                    </Typography>
-                    <Typography 
-                        variant="h6" 
-                        align="left" 
-                        color="text.secondary" 
-                    >
-                        Oops! Looks like you followed a bad link or the page hasn't been set up yet.
-                    </Typography>
-                    <Typography 
-                        variant="h6" 
-                        align="left" 
-                        color="text.secondary"
-                        paragraph
-                    >
-                        If you think this is a problem with us, please tell us
-                    </Typography>
-                    <Button variant="contained" sx={{ mt: 1 }}>
-                        Back home
-                    </Button>
+                        <Typography
+                            component="h1"
+                            variant="h1"
+                            align="left"
+                            color="text.primary"
+                        >
+                            404
+                        </Typography>
+                        <Typography 
+                            variant="h6" 
+                            align="left" 
+                            color="text.secondary" 
+                        >
+                            Oops! Looks like you followed a bad link or the page hasn't been set up yet.
+                        </Typography>
+                        <Typography 
+                            variant="h6" 
+                            align="left" 
+                            color="text.secondary"
+                            paragraph
+                        >
+                            If you think this is a problem with us, please tell us
+                        </Typography>
+                        <Button variant="contained" size="large" sx={{ mt: 1 }}>
+                            Back home
+                        </Button>
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6}>
