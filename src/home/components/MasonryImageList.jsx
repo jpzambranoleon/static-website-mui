a8d95a1a284012d34c243183e71235329e4427ab
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-04 15:34:54 -0500

    updated MasonryImageList.jsx

--- src/home/components/MasonryImageList.jsx
+++ src/home/components/MasonryImageList.jsx
@@ -6,23 +6,19 @@ const itemData = [
         title: 'plans'
     },
     {
-        img: '/images/develop.jpg',
-        title: 'develop'
-    },
-    {
-        img: '/images/nerds.jpg',
-        title: 'nerds'
-    },
-    {
         img: '/images/app_unsplash.jpg',
         title: 'app'
+    },
+    {
+        img: '/images/develop.jpg',
+        title: 'develop'
     },
 ]
 
 const MasonryImageList = () => {
     return (
-        <Box sx={{ width: '100%', height: '100%' }}>
-            <ImageList variant="masonry" cols={3} gap={8}>
+        <Box sx={{ width: '100%', height: '100px' }}>
+            <ImageList variant="masonry" cols={2} gap={8}>
                 {itemData.map((item) => (
                     <ImageListItem key={item.img}>
                         <img
