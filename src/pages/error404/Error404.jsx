cd7ed168dc11c82a3d93472dd4595ebd2d743b9d
Jean-Paul <jpzambrano.leon@gmail.com>
2022-06-04 17:46:07 -0500

    created Error404.jsx

---
+++ src/pages/error404/Error404.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+export default function Error404() {
+    return (
+        <main>
+            <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6, }}>
+                
+            </Box>
+        </main>
+    );
+  }
