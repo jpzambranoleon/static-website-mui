e3d562a5e0e6269f95b41e4fcb178d657abf9aa5
Jean-Paul <jpzambrano.leon@gmail.com>
2022-09-02 14:35:28 -0500

    fixed formatting

--- src/pages/home/components/MasonryImageList.jsx
+++ src/pages/home/components/MasonryImageList.jsx
@@ -1,37 +1,43 @@
 import { Box, ImageList, ImageListItem } from "@mui/material";
 
 const itemData = [
-    {
-        img: '/images/plans_crop.JPG',
-        title: 'plans'
-    },
-    {
-        img: '/images/app_crop.JPG',
-        title: 'app'
-    },
-    {
-        img: '/images/develop_crop.JPG',
-        title: 'develop'
-    },
-]
+  {
+    img: "/images/plans_crop.JPG",
+    title: "plans",
+  },
+  {
+    img: "/images/app_crop.JPG",
+    title: "app",
+  },
+  {
+    img: "/images/develop_crop.JPG",
+    title: "develop",
+  },
+];
 
 const MasonryImageList = () => {
-    return (
-        <Box sx={{ width: '100%', height: '100px', display: { xs: 'none', sm: 'block' } }}>
-            <ImageList variant="masonry" cols={2} gap={8}>
-                {itemData.map((item) => (
-                    <ImageListItem key={item.img}>
-                        <img
-                            src={`${item.img}?w=248&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100px",
+        display: { xs: "none", sm: "block" },
+      }}
+    >
+      <ImageList variant="masonry" cols={2} gap={8}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
+  );
+};
 
 export default MasonryImageList;
