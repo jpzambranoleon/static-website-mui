f36ae1eca6ff55fd95aac3ef00af1381cff51f7d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-06-23 18:24:14 -0500

    modified file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import { Box } from "@mui/material";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/styles.scss";
 import Copyright from "./components/Copyright";
 import Navbar from "./components/Navbar";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import Error404 from "./pages/error404/Error404";
-import Home from "./pages/home/Home";
+import Home from "./views/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/*" element={<Error404 />} />
         </Routes>
       </Router>
-      <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
-        <Copyright />
-      </Box>
     </div>
   );
 }
